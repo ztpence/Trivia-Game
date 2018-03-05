dd849b172d849209a198d2a0c0fc901caf95dd7a
Zane P <ztpence@gmail.com>
2018-03-04 17:40:13 -0700

    wrote out java map

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+
+// Need a few Variables 
+
+
+
+// Need an array to house trivia questions
+// Need to have sub array for possible answers and write question and correct answer
+
+var counter = 0;
+var triviaQuestions = [
+
+
+
+
+
+]
+
+//Need a way to select questions from array and diplay on page. Maybe a for loop with function 
+//or a method with array postion to pull
+
+
+// Game function by pressig button on first screen
+// Then questions will apper with counter
+// could use for loop maybe with html manipulation
+
+//need to create a way of keeping track of correct answers an incorrect answers
+//maybe if else statements to keep track of ansowers and an empty array to push them into 
+//need to display this text and that will be end of the game
+//Maybe have a resart function if time allows 
+
